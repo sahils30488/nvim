72a872accfde47e6a6b7da39ff100f0aa51209c6
sahils30488 <sahilsharma30488@gmail.com>
2025-05-05 18:14:05 +0530

    tab for completion

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
 	"Exafunction/codeium.vim",
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-a>", function()
+		vim.keymap.set("i", "<Tab>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<a-.>", function()
